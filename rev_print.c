278f12099ff5156d79b92076a8de8d74f4ca2566
Santiago Cardelle Montero <scardell@student.42urduliz.com>
2024-02-29 13:46:07 +0100

    mas funciones

--- rev_print.c
+++ rev_print.c
@@ -1,32 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*   rev_print.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: scardell <scardell@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/29 09:08:40 by scardell          #+#    #+#             */
-/*   Updated: 2024/02/29 12:50:06 by scardell         ###   ########.fr       */
+/*   Created: 2024/02/29 13:23:00 by scardell          #+#    #+#             */
+/*   Updated: 2024/02/29 13:29:56 by scardell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putstr(char *str)
+int main	(int argc, char **argv)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	while (str[i])
+	if (argc == 2)
 	{
-		write(1, &str[i], 1);
-		i++;
+		while (argv[1][i])
+			i++;
+		while (i--)
+			write(1, &argv[1][i], 1);
 	}
-}
-
-int main(void)
-{
-	char	string[] = "esto es una buena prueba";
-	ft_putstr(string);
+	write(1, "\n", 1);
 	return (0);
 }
+
