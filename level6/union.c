8ecdc9726a0e031e024e54b31f69c1a123db5d72
Santiago Cardelle <153379926+scardell@users.noreply.github.com>
2024-03-06 15:38:47 +0100

    Update union.c

--- level6/union.c
+++ level6/union.c
@@ -12,43 +12,38 @@
 
 #include <unistd.h>
 
-int check(int c, char *str, int index)
+int check(char *str, char c, int end)
 {
-	int i = 0;
-	
-	while(i < index)
+	int i;
+
+	i = 0;
+	while (str[i] && i < end)
 	{
 		if (str[i] == c)
-			return 0;
+			return (1);
 		i++;
 	}
-	return  1;
+	return  (0);
 }
 
 int main(int argc, char **argv)
 {	
 	int i = 0;
 	int j = 0;
-	int k = 0;
-	
+		
 	if (argc == 3)
 	{
-		while(argv[1][i] != '\0')
+		while(argv[1][i])
 		{
-			i++;
+			if (!check(argv[1], argv[1][i], i)
+				write(1, &argv[1][i], 1);
+				i++;
 		}
-		while(argv[2][j] != '\0')
+		while(argv[2][j])
 		{
-			argv[1][i] = argv[2][j];
-			i++;
-			j++;
-		}
-		i--;
-		while(k <= i)
-		{
-			if(check(argv[1][k], argv[1], k) == 1) 
-				write (1, &argv[1][k], 1); 
-			k++;
+			if (!check (argv[1], argv[2][j], i) && !scheck(argv[2], argv[2]][j], j))
+				write(1, &argv[2][j], 1);
+				j++;
 		}
 	}
 	write (1, "\n", 1);
