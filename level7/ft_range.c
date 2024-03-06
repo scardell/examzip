637a834c6502ba2e0d7bfcfa2222617c26b3625e
Santiago Cardelle Montero <scardell@student.42urduliz.com>
2024-03-06 10:35:43 +0100

    modificaciones

--- level7/ft_range.c
+++ level7/ft_range.c
@@ -6,7 +6,7 @@
 /*   By: scardell <scardell@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 15:54:19 by scardell          #+#    #+#             */
-/*   Updated: 2024/03/01 15:57:12 by scardell         ###   ########.fr       */
+/*   Updated: 2024/03/06 10:31:09 by scardell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,17 @@
 
 int        *ft_range(int start, int end)
 {
-        long        i;
-        long len;
+        long    i;
+        long    len;
+        int     *numbers;
 
+        if (end == start)
+                len = 1;
         if (end > start)
                 len = (long)end - (long)start + 1;
         if (start> end)
                 len = (long)start - (long)end + 1;
 
-        int *numbers;
         i = 0;
         numbers = (int *)malloc(sizeof(int) * len);
         if (!numbers)
@@ -38,17 +40,17 @@ int        *ft_range(int start, int end)
         return (numbers);
 }
 
-// #include <stdio.h>
+ #include <stdio.h>
 
-// int        main(void)
-// {
-//         // int start = -2;
-//         // int end = 2;
-//         int start = -2147483648;
-//         int end = 2147483647;
-//         int *prueba = ft_range(start, end);
-//         int i = 0;
-//         while (i < 4)
-//                 printf("%d |", prueba[i++]);
-//         return (0);
-// }
+int        main(void)
+{
+        int start = 0;
+        int end = 0;
+        //int start = -2147483648;
+        //int end = 2147483647;
+        int *prueba = ft_range(start, end);
+        int i = 0;
+        
+        printf("%d |", prueba[i++]);
+return (0);
+}
