fd71cf4491e64d0cac90553b67731c78dbd298b2
Santiago Cardelle Montero <scardell@student.42urduliz.com>
2024-03-06 13:17:14 +0100

    modificacion range y rrange

--- level7/ft_range.c
+++ level7/ft_range.c
@@ -6,7 +6,7 @@
 /*   By: scardell <scardell@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 15:54:19 by scardell          #+#    #+#             */
-/*   Updated: 2024/03/06 10:31:09 by scardell         ###   ########.fr       */
+/*   Updated: 2024/03/06 13:14:56 by scardell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,43 +14,40 @@
 
 int        *ft_range(int start, int end)
 {
-        long    i;
-        long    len;
-        int     *numbers;
+		long    i;
+		long    len;
+		int     *numbers;
 
-        if (end == start)
-                len = 1;
-        if (end > start)
-                len = (long)end - (long)start + 1;
-        if (start> end)
-                len = (long)start - (long)end + 1;
+		i = 0;
+		if (start > end)
+			len = (long)start - (long)end + 1;
+		else
+			len = (long)end - (long)start + 1;
 
-        i = 0;
-        numbers = (int *)malloc(sizeof(int) * len);
-        if (!numbers)
-                return (NULL);
+		numbers = (int *)malloc(sizeof(int) * len);
+		if (!numbers)
+			return (NULL);
 
-        while (i < len)
-        {
-                if (start <= end)
-                        numbers[i++] = start++;
-                else
-                        numbers[i++] = start--;
-        }
-        return (numbers);
+		while (i < len)
+		{
+			if (start <= end)
+				numbers[i++] = start++;
+			else
+				numbers[i++] = start--;
+		}
+		return (numbers);
 }
 
  #include <stdio.h>
 
 int        main(void)
 {
-        int start = 0;
-        int end = 0;
-        //int start = -2147483648;
-        //int end = 2147483647;
-        int *prueba = ft_range(start, end);
-        int i = 0;
-        
-        printf("%d |", prueba[i++]);
+	int *prueba;
+		
+	prueba = ft_range(-2147483648, 2147483647);
+	printf("%d\n", prueba[0]);
+	printf("%d\n", prueba[1]);
+	printf("%d\n", prueba[2]);
+	printf("%d\n", prueba[3]);
 return (0);
 }
--- level7/ft_rrange.c
+++ level7/ft_rrange.c
@@ -6,34 +6,48 @@
 /*   By: scardell <scardell@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 15:54:51 by scardell          #+#    #+#             */
-/*   Updated: 2024/03/01 15:57:56 by scardell         ###   ########.fr       */
+/*   Updated: 2024/03/06 13:15:22 by scardell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int *ft_rrange(int start, int end)
+#include <stdlib.h>
+
+int	*ft_rrange(int start, int end)
 {
-	int *range;
-	int i = 0;
-	int step = 1;
-	int n = end - start;
+	long len;
+	long	i;
+	int *numbers;
+	
+	i = 0;
+	if (start > end)
+		len = (long)start - (long)end + 1;
+	else
+		len = (long)end - (long)start + 1;
 
-	if (n < 0)
-		(n *= -1);
-	n++;
-
-	range = (int *)malloc(sizeof(int) * n);
-	if (range)
+	numbers = (int *)malloc(sizeof(int) * len);
+	if (!numbers)
+		return (NULL);
+	while (i < len)
 	{
 		if (start < end)
-			step = -1;
-		while (i < n)
-		{
-			range[i] = end;
-			end = end + step;
-			i++;
-		}
+			numbers[i++] = end--;
+		else
+			numbers[i++] = end++;
 	}
-	return (range);
+	return (numbers);
+}
+
+ #include <stdio.h>
+ int        main(void)
+{
+	int *prueba;
+		
+	prueba = ft_rrange(-2147483648, 2147483647);
+	printf("%d\n", prueba[0]);
+	printf("%d\n", prueba[1]);
+	printf("%d\n", prueba[2]);
+	printf("%d\n", prueba[3]);
+return (0);
 }
