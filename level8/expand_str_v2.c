3fbd6d345e2002924b594adf7f04b33f41f6aaf1
Santiago Cardelle Montero <scardell@student.42urduliz.com>
2024-03-08 12:40:09 +0100

    modificacion

---
+++ level8/expand_str_v2.c
@@ -0,0 +1,34 @@
+
+#include <unistd.h>
+
+int space(int i)
+{
+	return (i == ' ' || (i >= 9 && i <= 13));
+}
+
+int printable(int i)
+{
+	return (i >= 33 && i <= 126);
+}
+
+int main (int argc, char **argv)
+{
+	int i;
+
+	i = 0;
+	if (argc ==2)
+	{
+		while(space(argv[1][i]))
+			i++;
+		while(argv[1][i])
+		{
+			if (printable(argv[1][i]))
+				write(1, &argv[1][i], 1);    
+			if (space(argv[1][i]) && printable(argv[1][i+1]))
+				write(1, "   ", 3);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
--- level8/ft_split.c
+++ level8/ft_split.c
@@ -6,12 +6,11 @@
 /*   By: scardell <scardell@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 15:56:29 by scardell          #+#    #+#             */
-/*   Updated: 2024/03/05 12:44:38 by scardell         ###   ########.fr       */
+/*   Updated: 2024/03/07 07:09:48 by scardell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <stdio.h>
 
 int	ft_isspace(int i)
 {
@@ -47,15 +46,17 @@ char	**ft_split(char *str)
 		return (out);
 }
 
+#include <stdio.h>
+
 int main()
 {
-	char *str = "   hola   que tal   estas";
-	char **out = ft_split(str);
-	int i = 0;
-	while (out[i])
-	{
-		printf("%s\n", out[i]);
-		i++;
-	}
+	
+	char **out = ft_split("   hola   que tal   estas");
+	
+	printf("%s\n", out[0]);
+	printf("%s\n", out[1]);	
+	printf("%s\n", out[2]);
+	printf("%s\n", out[3]);
+
 	return (0);
 }
